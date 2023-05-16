5e0178103d3433f54d5a91b68572b9681c00efcc
Guyddo <guyddogl@gmail.com>
2023-05-15 22:17:00 -0300

    Requirement 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,25 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
 # Req 2
 def test_dish():
-    pass
+    test_instance = Dish("Churrasco", 28.9)
+
+    assert test_instance == Dish("Churrasco", 28.9)
+    assert test_instance.name == "Churrasco"
+    assert hash(test_instance) == hash("Dish('Churrasco', R$28.90)")
+    assert not test_instance.get_restrictions()
+
+    with pytest.raises(
+        ValueError, match="Dish price must be greater then zero."
+    ):
+        Dish("Pizza", 0)
+
+    test_instance.add_ingredient_dependency(Ingredient("Queijo"), 1)
+
+    assert test_instance.recipe == {Ingredient("Queijo"): 1}
+    assert {
+        ingredient.name for ingredient in test_instance.get_ingredients()
+    } == {"Queijo"}
