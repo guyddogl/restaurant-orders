8f7a61611f04b725b8bdf4eb2553f914b145491b
Guyddo <guyddogl@gmail.com>
2023-05-15 21:35:12 -0300

    Requirement 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,12 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-#
 # Req 1
 def test_ingredient():
-    pass
+    test_instance = Ingredient("chocolate")
+
+    assert repr(test_instance) == "Ingredient('chocolate')"
+    assert hash(test_instance) == hash("chocolate")
+    assert test_instance.name == "chocolate"
+    assert test_instance == Ingredient("chocolate")
+    assert not test_instance.restrictions
